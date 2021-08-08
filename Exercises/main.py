63494d13dee9ac62df92ec1e1fe324341e5afee2
Ozioma <oziomaokoroafor@gmail.com>
2021-08-08 01:36:39 +0100

    First Python commit

---
+++ Exercises/main.py
@@ -0,0 +1,64 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+    import keyword
+    print(keyword.kwlist)
+
+    first_number = 1
+    print(first_number)
+
+    my_money = 1000.00
+    print(my_money)
+
+    x = 5
+    y = 2
+
+    z = (x + x - y ** 2)
+    print(z)
+
+    x, y = 8, 5
+
+    r = x//y
+
+    print(r)
+print("""Read and hustle ooo
+let me dance""")
+greeting = ' Hello\'s'
+
+print(greeting *5)
+
+
+guy_man = 'say cheese'
+
+print('when taking pictures', guy_man, 'always')
+
+owner = 'Lawrende Ferlinghetti'
+age = 100
+print('the name of the man is, {} and he is {} years old' .format(owner, age))
+print('Say {} everytime they walk in' .format(guy_man))
+
+print(len(guy_man))
+
+name = 'Corey'
+
+print(name.lower())
+
+print(name.count('o'))
+
+print(type('5' + '7'))
+
+print('5' + '7')
+
+print(5+7)
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
